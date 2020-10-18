dca07ae5c50132fdbca3407c48187d41f52b7ce0
Antonio Carlos Peron Junior <antonio.peron@cmc.pr.gov.br>
2020-10-18 10:13:20 -0300

    Laco For

---
+++ quartoCapitulo/exemplos/lacoFor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    for (int i=0; i<100; ++i){
+        cout << i << '\t' << pow(i, 2) << '\n';
+    }
+
+    return 0;
+}
